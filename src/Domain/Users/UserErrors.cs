b94bfb0530edc25e6e137eb12c48263863b340c8
jnabin <jahangirnabin2@gmail.com>
2025-07-01 00:36:15 +0600

    added user errors

---
+++ src/Domain/Users/UserErrors.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Abstractions;
+
+namespace Domain.Users
+{
+    public static class UserErrors
+    {
+        public static Error UserNotFound =
+            new Error("User.Found", "The user with the specified identifier was not found");
+
+        public static Error InvalidCredentials =
+            new Error("User.InvalidCredentials", "The provided credentials were invalid");
+    }
+}
