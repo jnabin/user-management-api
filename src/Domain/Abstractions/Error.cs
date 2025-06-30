e61c5b996dd9b85e2fcf2c8f0c51a1f9c8292871
jnabin <jahangirnabin2@gmail.com>
2025-07-01 00:30:58 +0600

    completed domain with domain driven approch

--- src/Domain/Abstractions/Error.cs
+++ src/Domain/Abstractions/Error.cs
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 
 namespace Domain.Abstractions
 {
-    public class Error
+    public record Error(string code, string name)
     {
+        public static Error None = new Error(string.Empty, string.Empty);
+        public static Error NullValue = new Error("Error.NullValue", "Null value was provided");
     }
 }
