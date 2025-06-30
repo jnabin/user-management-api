979080a88b2604897025027c0c8ed9eb755c54d3
jnabin <jahangirnabin2@gmail.com>
2025-07-01 00:00:55 +0600

    adding domain woth interfaces

---
+++ src/Domain/Users/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Users
+{
+    public interface IUserRepository
+    {
+        Task<IEnumerable<User>> GetAllAsync();
+        Task<User?> GetByIdAsync(Guid id, CancellationToken token = default);
+        void Add(User user);
+    }
+}
---
+++ src/Domain/Users/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Abstractions;
+
+namespace Domain.Users
+{
+    public sealed class User : Entity
+    {
+        private User(Guid id, Email email, FirstName firstName, LastName lastName)
+            :base(id)
+        {
+            Email = email;
+            FirstName = firstName;  
+            LastName = lastName;
+        }
+        public Email Email { get; init; }
+        public FirstName FirstName { get; init; }
+        public LastName LastName { get; init; }
+    }
+}
