3d9096bb27302d922bfb215bb0b8baf71c28b4ec
jnabin <jahangirnabin2@gmail.com>
2025-07-03 02:59:47 +0600

    created unit test for user domain

---
+++ test/Domain.UnitTests/Users/UserData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Users;
+
+namespace Domain.UnitTests.Users
+{
+    internal static class UserData
+    {
+        public static readonly FirstName FirstName = new FirstName("Jn");
+        public static readonly LastName LastName = new LastName("Nabin");
+        public static readonly Email Email = new Email("jnabin2@gmail.com");
+    }
+}
---
+++ test/Domain.UnitTests/Users/UserTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Users;
+using FluentAssertions;
+
+namespace Domain.UnitTests.Users
+{
+    public class UserTests
+    {
+        [Fact]
+        public void Create_SHould_SetPropertyValues()
+        {
+            //Arrange
+
+            //Act
+            var user = User.Create(UserData.Email, UserData.FirstName, UserData.LastName);
+
+            //Assert
+            user.FirstName.Should().Be(UserData.FirstName);
+            user.LastName.Should().Be(UserData.LastName);
+            user.Email.Should().Be(UserData.Email);
+        }
+    }
+}
